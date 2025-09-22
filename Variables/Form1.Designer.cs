e87f3fd8213b032d59edb863ef0f4eeabddc3fe5
Kevinech <kevinemch@uamv.edu.ni>
2025-09-22 07:51:08 -0600

    Quedamos a medias, continuamos el miercoles

--- Variables/Form1.Designer.cs
+++ Variables/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbCodigo = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lblVariableLocal = new System.Windows.Forms.Label();
+            this.lblVariableGlobal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Codigo:";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(116, 41);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 20);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // tbCodigo
+            // 
+            this.tbCodigo.Location = new System.Drawing.Point(113, 89);
+            this.tbCodigo.Name = "tbCodigo";
+            this.tbCodigo.Size = new System.Drawing.Size(100, 20);
+            this.tbCodigo.TabIndex = 3;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(104, 140);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(112, 39);
+            this.btnVerificar.TabIndex = 4;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lblVariableLocal
+            // 
+            this.lblVariableLocal.AutoSize = true;
+            this.lblVariableLocal.Location = new System.Drawing.Point(63, 220);
+            this.lblVariableLocal.Name = "lblVariableLocal";
+            this.lblVariableLocal.Size = new System.Drawing.Size(70, 13);
+            this.lblVariableLocal.TabIndex = 5;
+            this.lblVariableLocal.Text = "Variable local";
+            // 
+            // lblVariableGlobal
+            // 
+            this.lblVariableGlobal.AutoSize = true;
+            this.lblVariableGlobal.Location = new System.Drawing.Point(66, 270);
+            this.lblVariableGlobal.Name = "lblVariableGlobal";
+            this.lblVariableGlobal.Size = new System.Drawing.Size(78, 13);
+            this.lblVariableGlobal.TabIndex = 6;
+            this.lblVariableGlobal.Text = "Variable Global";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(503, 387);
+            this.Controls.Add(this.lblVariableGlobal);
+            this.Controls.Add(this.lblVariableLocal);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.tbCodigo);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbCodigo;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label lblVariableLocal;
+        private System.Windows.Forms.Label lblVariableGlobal;
     }
 }
 
